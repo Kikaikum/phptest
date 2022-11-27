639ffd9fc30fccaf2745facbcbfaec7a9a64561e
Kikaikum <enriquecabrera92@gmail.com>
2022-11-27 15:00:15 +0100

    test

--- index.php
+++ index.php
@@ -10,7 +10,7 @@
 <body>
 	<div>
 	
-	<button onclick="document.location='bar/anadirproducto.php'">Añasd Producto</button></br>
+	<button onclick="document.location='bar/anadirproducto.php'">15:00</button></br>
 	<button onclick="document.location='bar/editarProductos.php'">Editar Productos</button></br>
 	<button onclick="document.location='cliente/indexcliente.php'">Cliente</button></br>
 	<button onclick="document.location='bar/cocina.php'">Cocina</button></br>
