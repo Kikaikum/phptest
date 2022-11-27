618a035d678f0660449ec6809f2689b7f464e962
Kikaikum <enriquecabrera92@gmail.com>
2022-11-27 14:41:02 +0100

    test

---
+++ index.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<title>BARPP</title>
+	<link rel="stylesheet" href="Style.css">
+	
+</head>
+<body>
+	<div>
+	
+	<button onclick="document.location='bar/anadirproducto.php'">Añadir Producto</button></br>
+	<button onclick="document.location='bar/editarProductos.php'">Editar Productos</button></br>
+	<button onclick="document.location='cliente/indexcliente.php'">Cliente</button></br>
+	<button onclick="document.location='bar/cocina.php'">Cocina</button></br>
+	<button onclick="document.location='bar/camarero.php'">Camarero</button></br>
+	<button onclick="document.location='bar/crearmesa.view.php'">crear mesa</button></br>
+	<button onclick="document.location='bar/cobrarmesa.php'">cobrar</button></br>
+	<button onclick="document.location='bar/editarpedido.view.php'">editar pedido</button></br>
+	<button onclick="document.location='bar/verMesas.php'">Ver Mesas</button></br>
+	<button onclick="document.location='bar/pedir.php'">Pedir</button></br>
+	
+                 
+	</div>
+</body>
